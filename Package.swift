7ad08f2c75ea91d8a52532d070a0625908f0b12e
sumer-R <fbwjdgk00@gmail.com>
2025-11-03 11:35:47 +0900

    Package name 변경

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "McWebview",
+    name: "McL-McWebview",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
